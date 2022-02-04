f8d6383324832d40df0a8c5f522e015927130c3c
sarthak arora <f20200060@pilani.bits-pilani.ac.in>
2022-02-04 12:03:00 +0530

    loader applied on other pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+window.onload = function () {
+  document.getElementsByClassName("loader")[0].style.display = "none";
+}
+
 const $menuBtn = document.querySelector(".menu-btn");
 let isMenuOpen = false;
 $menuBtn.addEventListener("click", () => {
@@ -20,7 +24,4 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-}
-window.onload = function () {
-  document.getElementsByClassName("loader")[0].style.display = "none";
 }
